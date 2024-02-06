a0b0867f54f77f674a60d577dbd08cca5fb0af40
Hamide <gozenhmd2002@gmail.com>
2024-02-06 16:56:48 +0300

    String Concatenation

---
+++ Exercise3.2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String x, y, z;
+        x = "CENG2003";
+        y = "OOP with JAVA";
+        z = x + "-" + y;
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("Z: " + z);
+    }
+}
