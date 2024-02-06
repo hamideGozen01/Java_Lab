d1d0033264ba8a6be5580acd910ce1a1f615669e
Hamide <gozenhmd2002@gmail.com>
2024-02-06 16:53:09 +0300

    Character Deviations

---
+++ Exercise2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\Quoted from Huckleberry Finn\\");
+        System.out.println("I broke in and says: ");
+        System.out.println("\"They're in an awful peck of trouble, and\"");
+        System.out.println("\"Who is?\"");
+        System.out.println("\"Why, pap and mam and sis and Miss Hooker; \n\t and if you'd take ferryboat and go up there\"");
+        System.out.println("\"Up where? Where are they?\"");
+        System.out.println("\"On the wreck.\"");
+        System.out.println("\"What wreck?\"");
+    }
+}
